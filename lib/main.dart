453ddecbcddc91d5865798c604e180718e8b29b1
king0404 <xhakxh147@handong.ac.kr>
2022-07-14 15:57:06 +0900

    test push

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Demo',
+      title: 'Dem  o',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
